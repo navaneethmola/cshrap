edcf82c48ab0118d3bb64ec5ddc9070f9582eb1a
navaneeth mola <navaneethmola@gmail.com>
2021-11-10 11:47:11 +0530

    class

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cshrap
+{
+    class Customer
+    {
+        string _firstname;
+        string _lastname;
+        public Customer(string firstname,string lastname )
+        {
+            _firstname=firstname;
+            _lastname=lastname;
+        }
+        public string PrintFullName()
+        {
+            return _firstname +" "+ _lastname;
+        }
+        ~Customer()
+        {
+
+        }
+class program
+{
+
+        static void Main()
+        {
+          Customer c1=new Customer("navaneeth","mola");
+          Console.WriteLine(c1.PrintFullName());  
+        }
+    }
+}
+}
